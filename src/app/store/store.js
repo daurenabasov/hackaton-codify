8d59e623d53ae4a7dd4d4a957f7cd2d5580ad6af
daurenabasov <daurenabasov987@gmail.com>
2023-06-18 05:31:45 +0600

    fix conflicts

--- src/app/store/store.js
+++ src/app/store/store.js
@@ -1,8 +1,6 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit"
 import registrationReducer from "./thunks/sign-up/sign-up.slice"
 import authReducer from "./thunks/sign-in/sign-in.slice"
-const rootReducer = combineReducers({
-    signUp: registrationReducer
 
 const rootReducer = combineReducers({
     signIn: registrationReducer,
