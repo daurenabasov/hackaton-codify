72521ead511d2797e742c2fa7597942a8f0132fa
daurenabasov <daurenabasov987@gmail.com>
2023-06-17 15:51:52 +0600

    add env config and axios

---
+++ src/config/variables-env.js
@@ -0,0 +1,11 @@
+const getEnvVar = (key) => {
+    if (import.meta.env.VITE_REACT_APP_API_URL === undefined) {
+        throw new Error(`Env variable ${key} is required`);
+    }
+    return import.meta.env.VITE_REACT_APP_API_URL || "";
+};
+
+export const API_URL = getEnvVar("VITE_REACT_APP_API_URL");
+
+
+
