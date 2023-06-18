68a4dba3711e9f032d21fbc5e2bc77e8663c9c75
daurenabasov <daurenabasov987@gmail.com>
2023-06-18 12:07:06 +0600

    save

--- src/pages/auth/SignIn.jsx
+++ src/pages/auth/SignIn.jsx
@@ -5,6 +5,7 @@ import { signInFetch } from "../../app/store/thunks/sign-in/sign-in.thunk";
 import { useNavigate } from "react-router-dom";
 import { Navbar } from "../../components";
 import styles from "../../style";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignIn = () => {
   const [email, setEmail] = useState("");
@@ -39,32 +40,49 @@ const SignIn = () => {
           <div className={`${styles.boxWidth}`}>
             <Navbar />
           </div>
-        </div>
-        <div className="login-box">
-          <h2>Authorization</h2>
-          <form onSubmit={handleSubmit}>
-            <div className="user-box">
-              <input
-                type="email"
-                name=""
-                value={email}
-                onChange={handleChange(setEmail)}
-                required=""
-              />
-              <label>Email</label>
-            </div>
-            <div className="user-box">
-              <input
-                type="password"
-                name=""
-                value={password}
-                onChange={handleChange(setPassword)}
-                required=""
-              />
-              <label>Password</label>
-            </div>
-            <button type="submit">SIGN IN</button>
-          </form>
+          <div className="login-box">
+            <Link to="/">Go home</Link>
+
+            <h2>Authorization</h2>
+            <form onSubmit={handleSubmit}>
+              <div className="user-box">
+                <input
+                  type="email"
+                  name=""
+                  value={email}
+                  onChange={handleChange(setEmail)}
+                  required=""
+                />
+                <label>Email</label>
+              </div>
+              <div className="login-box">
+                <h2>Authorization</h2>
+                <form onSubmit={handleSubmit}>
+                  <div className="user-box">
+                    <input
+                      type="email"
+                      name=""
+                      value={email}
+                      onChange={handleChange(setEmail)}
+                      required=""
+                    />
+                    <label>Email</label>
+                  </div>
+                  <div className="user-box">
+                    <input
+                      type="password"
+                      name=""
+                      value={password}
+                      onChange={handleChange(setPassword)}
+                      required=""
+                    />
+                    <label>Password</label>
+                  </div>
+                  <button type="submit">SIGN IN</button>
+                </form>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </>
